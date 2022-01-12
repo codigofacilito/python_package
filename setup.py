8d5a78499d4b70f0b77c709e51f05359023a67dd
eduardogpg <eduardo78d@gmail.com>
2022-01-12 11:58:02 -0600

    First commit

---
+++ setup.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+from setuptools import setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Description'
+PACKAGE_NAME = 'Package Name'
+AUTHOR = ''
+EMAIL = ''
+GITHUB_URL = ''
+
+setup(
+    name = PACKAGE_NAME,
+    packages = [DESCRIPTION],
+    version = VERSION,
+    license='MIT',
+    description = DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    author = AUTHOR,
+    author_email = EMAIL,
+    url = GITHUB_URL,
+    keywords = [],
+    install_requires=[ 
+        'requests',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
