e5c98611046c50341e8ac2f3450ab949bf72313c
aleco <66926752+alecorodri@users.noreply.github.com>
2022-11-16 23:52:28 -0500

    add first test

--- codfacapipy/workshops.py
+++ codfacapipy/workshops.py
@@ -1,6 +1,11 @@
 import requests
 
 def unreleased():
+
+    """retornar los proximos talleres en codigo facilito
+    >>> type(unreleased()) == type(dict())
+    true
+    """
     response = requests.get('https://codigofacilito.com/api/v2/workshops/unreleased')
 
     if response.status_code == 200:
